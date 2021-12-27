d83a887c60ddbcbaa72fc0745481189dc0486b4d
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-27 00:21:04 -0800

    Iniate answering the prompt

--- server.js
+++ server.js
@@ -1,33 +1,35 @@
-// server.js
-// where your node app starts
-
-// init project
 require('dotenv').config();
 var express = require('express');
 var app = express();
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
-app.get("/api/hello", (req, res) => {
+app.get('/api/hello', (req, res) => {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/whoami', (req, res) => {
+  let infoObject;
+  let ipidress, lan, software;
+  infoObject = { ipidress, lan, software}
 
+  res.json({...infoObject});
+})
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, () => {
   console.log('Your app is listening on port ' + listener.address().port);
 });
+
+
+// A request to / api / whoami should return a JSON object with your IP address in the ipaddress key.
+
+// A request to / api / whoami should return a JSON object with your preferred language in the language key.
+
+// A request to / api / whoami should return a JSON object with your software in the software key.
